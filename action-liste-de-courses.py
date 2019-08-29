220318aff62cedc1d9158e4ee00af77d788b7297
Loky31 <bensurv31@gmail.com>
2019-08-29 16:08:16 +0200

    Add files via upload

--- action-liste-de-courses.py
+++ action-liste-de-courses.py
@@ -5,11 +5,20 @@ import json
 import configparser
 import io
 import requests
+import telegram
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 
 state = {'confirmationPurge': False}
 
+config = read_configuration_file()
+my_token = 'config['secret']['TOKEN']'
+my_chat_id= 'config['secret']['CHAT_ID']'
+
+#liste = load_list()
+#if not liste:
+        #return "La liste de courses est vide"
+#my_msg = '"Liste de courses: {}".format(", ".join(liste))'
 
 class SnipsConfigParser(configparser.SafeConfigParser):
     def to_dict(self):
@@ -79,6 +88,9 @@ def del_list():
     return "J'ai purgé la liste de courses"
 
 
+def send_telegram(msg,chat_id,token):
+    bot = telegram.Bot(token=token)
+    bot.sendMessage(chat_id=chat_id, text=msg)
 
 def send():
     liste = load_list()
@@ -100,11 +112,11 @@ def send():
 #        return "Telegram ne répond pas"
 
     #code = response.status_code
-#    if response == telegramData[2]:
-    #return "J'ai envoyé la liste de courses par Telegram"
-#    else:
-#            return "Oups!!!! Ca n'a pas marché"
-
+    if "\"ok\":true" in response:
+        return "J'ai envoyé la liste de courses par Telegram"
+    else:
+        return "Oups, Ca n'a pas marché"  
+    
 def intent_callback(hermes, intent_message):
     intent_name = intent_message.intent.intent_name.replace("Loky31:", "")
     result = None
